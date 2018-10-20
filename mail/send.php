02ab3ba950d0ecfdf666ed7dfd361c1280ecfdd8
Sayeed169 <sayeedhasan169@gmail.com>
2018-10-21 01:15:03 +0600

    mail sending works just fine

--- mail/send.php
+++ mail/send.php
@@ -9,17 +9,17 @@ require './PHPMailer/src/SMTP.php';
 $mail = new PHPMailer;
 $mail->isSMTP(); 
 $mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
-$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
-$mail->Port = 587; // TLS only
+$mail->Host = "mail.humanlibrarybd.org"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+$mail->Port = 26; // TLS only
 $mail->SMTPSecure = 'tls'; // ssl is depracated
 $mail->SMTPAuth = true;
-$mail->Username = $smtpUsername;
-$mail->Password = $smtpPassword;
+$mail->Username = "info@humanlibrarybd.org";
+$mail->Password = "Unt1tled!$";
 // $mail->setFrom($emailFrom, $emailFromName);
-$mail->setFrom("sayeedhasan169@gmail.com", "Sayeed Hasan");
+$mail->setFrom("info@humanlibrarybd.org", "Sayeed Hasan");
 // $mail->addAddress($emailTo, $emailToName);
-$mail->addAddress("hasan.sayeed@bjitgroup.com", "Hasan Sayeed");
-$mail->Subject = 'PHPMailer GMail SMTP test';
+$mail->addAddress("sayeedhasan169@gmail.com", "Hasan Sayeed");
+$mail->Subject = 'HLD Message';
 $mail->msgHTML("test body"); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
 $mail->AltBody = 'HTML messaging not supported';
 // $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
